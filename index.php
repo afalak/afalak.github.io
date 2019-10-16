114229e9b330bc3a80dd33a4cd51b1a657e508a0
Falak Ahmed <afalak@users.noreply.github.com>
2019-10-16 08:53:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
  <head>
-  <meta http-equiv="Refresh" content="7; url=/index.html" />
+  <meta http-equiv="Refresh" content="3; url=/index.html" />
   <title>Falak</title>
  </head>
  <body>
-  <p>Please follow <a href="/index.html">this link</a>.
+  <p>Please follow <a href="/index.html">this link</a>
   </body>
 </html>
