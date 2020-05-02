3d90710803d7c81e5aee8ca751868b6286c40754
afalak-1 <afalak@gmail.com>
2020-05-02 12:06:47 +0000

    sync added

---
+++ synch.sh
@@ -0,0 +1,9 @@
+	cp resume.html index.html
+	echo '<br>' >> index.html
+	date >> index.html
+	cp DetailResume.html indexD.html
+	echo '<br>' >> indexD.html
+	date >> indexD.html
+	cat readme.txt > README.md
+	date >> README.md
+	git status
